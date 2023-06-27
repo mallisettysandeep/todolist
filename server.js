14a129e78ceb96a908d4cb200493bed997c79290
malisettysandeep <malisettysandeep2000@gmail.com>
2023-06-27 18:44:41 +0530

    mern

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const TaskSchema=require('./model');
+const cors=require('cors');
+const app=express();
+app.use(express.json())
+app.use(cors({
+    origin:"*"
+
+}))
+
+mongoose.connect("mongodb+srv://sandeep:sandeep@cluster0.gwjglfc.mongodb.net/?retryWrites=true&w=majority").then(
+()=>console.log("db connected")
+).catch(err=>console.log(err))
+
+
+app.post('/addtask',async(req,res)=>{
+    const {todo}=req.body;
+    try{
+        const newData=new TaskSchema({
+            todo:todo
+        })
+        await newData.save();
+        return res.json(await TaskSchema.find())
+
+       
+
+
+    }
+    catch(err){
+        console.log(err.message);
+    }
+
+
+})
+app.get('/gettask',async(req,res)=>{
+    try{
+        return res.json(await TaskSchema.find());
+
+    }
+    catch(err){
+        console.log(err.message);
+
+    }
+})
+app.delete('/delete/:id',async(req,res)=>{
+    try{
+        await TaskSchema.findByIdAndDelete(req.params.id)
+        return res.json(await TaskSchema.find())
+
+    }
+    catch(err){
+        console.log(err.message);
+
+    }
+})
+
+
+
+
+
+app.listen(4000,()=>console.log("server connected"))
